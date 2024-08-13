596123e220d82454db9fdbdfc9f1631bdda5652d
Matthew Leach <matthew@engagexr.co>
2024-08-13 10:10:28 +0100

    Removed reference to UnityEngine

--- UPMPackage/Scripts/Runtime/Char32Utils.cs
+++ UPMPackage/Scripts/Runtime/Char32Utils.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine;
 
 namespace RTLTMPro {
     public static class Char32Utils {
